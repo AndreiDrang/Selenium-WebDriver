ad2505bbb4f7192825a423ce9d08b62fdba42aea
AndreiDrang <drang.andray@gmail.com>
2016-08-01 15:44:52 +0300

    Update test_bonuses_to_employees.py

--- test_bonuses_to_employees.py
+++ test_bonuses_to_employees.py
@@ -4,11 +4,8 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 
 class BonusTest(unittest.TestCase):
-	#Страница с бонусами персоналу
-	ALL_BONUSES = '//div[1]//div[2]//li[4]/a'
 	#Добавление новой премии
 	NEW_BONUS = '/html/body/div[2]/ul/li[2]/a'
 	#Список сотрудников
@@ -40,9 +37,10 @@ class BonusTest(unittest.TestCase):
 	##Переменная с дробной частью записанная через точку, для редактирования старой записи
 	FRACTIONAL_NUMBER_COMMA_EDIT = '-37,98'
 
+
 	# открываем браузер, переходим на страницу
 	def setUp(self):
-		self.driver = webdriver.Firefox()
+		self.driver = webdriver.Chrome()
 		self.driver.implicitly_wait(30)
 		self.driver.maximize_window()
 		self.driver.get('http://suok.ezeev.ru/llc/#')
@@ -54,29 +52,36 @@ class BonusTest(unittest.TestCase):
 		self.driver.find_element_by_id('employee_password').send_keys(password)
 		self.driver.find_element_by_xpath("//button[@type='submit']").submit()
 
+	#Функция проверки налчичя не подписанной премии и её архивация
 	def Checking(self):
-		self.driver.implicitly_wait(2)
-		self.driver.find_element_by_link_text(u"Suok").click()
+		#Ожидание элемента
+		self.driver.implicitly_wait( 0.5 )
+		# Переходим в раздел под названием Suok
+		self.driver.find_element_by_link_text( u"Suok" ).click( )
 		assert u"Suok" in self.driver.title
-		self.driver.find_element_by_link_text(u'Премии').click()
+		# Переход в раздел с премиями
+		self.driver.find_element_by_link_text( u'Премии' ).click( )
+		#Открываем форму с премией
 		self.driver.find_element_by_xpath(self.BONUS).click()
+		#Пытаемся отправить форму в архив
 		try:
 			# Архивация премии
-			self.driver.find_element_by_partial_link_text(self.IN_ARCHIVE).click()
+			self.driver.find_element_by_partial_link_text( self.IN_ARCHIVE ).click()
 			# Переключение на всплывающее окно  и подтверждение действия по архивации премии
 			self.driver.switch_to.alert.accept()
 		except:
 			self.driver.back()
 
+
 	def Bonus_Creating(self):
-		self.driver.find_element_by_xpath(self.ALL_BONUSES).click()
+		#Переход  в раздел с добавлением новой премии сотруднику
 		self.driver.find_element_by_xpath(self.NEW_BONUS).click()
-		time.sleep(2)
+		#Открытие списка работников
 		self.driver.find_element_by_xpath(self.LIST_OF_EMPLOYEES).click()
-		time.sleep(1)
+		#Выбор нужного работника
 		self.driver.find_element_by_xpath(self.WORKER).click()
+		# Переход в форму для заполнения данных по выплатам
 		self.driver.find_element_by_xpath(self.NEXT).click()
-		time.sleep(1)
 		assert u'Расчет премии' in self.driver.title
 
 	#Заполнение полей данными
@@ -92,11 +97,11 @@ class BonusTest(unittest.TestCase):
 
 	#Отрицательное значение в сумму начета
 	def test_negative_summ(self):
+		self.Checking()
 		self.Bonus_Creating()
 		self.Bonus_Filling(self.NEGATIVE_BONUS)
 
 		#Проверяем сохранилась ли преми и не появилось ли ошибок
-		time.sleep(0.5)
 		self.assertNotEqual(self.driver.title, u'Расчет премии')
 
 		# Переход к ранее созданной премии
@@ -114,7 +119,6 @@ class BonusTest(unittest.TestCase):
 		self.Bonus_Filling(self.NEGATIVE_BONUS_EDIT)
 
 		#Проверяем сохранилась ли преми и не появилось ли ошибок
-		time.sleep(0.5)
 		self.assertNotEqual(self.driver.title, u'Расчет премии')
 
 		time.sleep(1)
@@ -129,16 +133,15 @@ class BonusTest(unittest.TestCase):
 		#Архивация премии
 		self.driver.find_element_by_partial_link_text(self.IN_ARCHIVE).click()
 		#Переключение на всплывающее окно  и подтверждение действия по архивации премии
-		time.sleep(1)
 		self.driver.switch_to.alert.accept()
 
 	#Проверка дробного числа записанного с точкой
 	def test_fractional_number_dot(self):
+		self.Checking( )
 		self.Bonus_Creating()
 		self.Bonus_Filling(self.FRACTIONAL_NUMBER_DOT)
 
 		# Проверяем сохранилась ли преми и не появилось ли ошибок
-		time.sleep(0.5)
 		self.assertNotEqual(self.driver.title, u'Расчет премии')
 
 		# Переход к ранее созданной премии
@@ -157,10 +160,8 @@ class BonusTest(unittest.TestCase):
 		self.Bonus_Filling(self.FRACTIONAL_NUMBER_DOT_EDIT)
 
 		# Проверяем сохранилась ли преми и не появилось ли ошибок
-		time.sleep(0.5)
 		self.assertNotEqual(self.driver.title, u'Расчет премии')
 
-		time.sleep(1)
 		# Извлечение значения строки с премией
 		SUMM_AMOUNT = self.driver.find_element(By.ID, self.SUMM_OF_BONUS).get_attribute('value')
 		# Извлечение значения
@@ -173,16 +174,15 @@ class BonusTest(unittest.TestCase):
 		# Архивация премии
 		self.driver.find_element_by_partial_link_text(self.IN_ARCHIVE).click()
 		# Переключение на всплывающее окно  и подтверждение действия по архивации премии
-		time.sleep(1)
 		self.driver.switch_to.alert.accept()
 
 	#Проверка дробного числа записанного с запятой
 	def test_fractional_number_comma(self):
+		self.Checking( )
 		self.Bonus_Creating()
 		self.Bonus_Filling(self.FRACTIONAL_NUMBER_COMMA)
 
 		# Проверяем сохранилась ли преми и не появилось ли ошибок
-		time.sleep(0.5)
 		self.assertNotEqual(self.driver.title, u'Расчет премии')
 
 		# Переход к ранее созданной премии
@@ -201,10 +201,8 @@ class BonusTest(unittest.TestCase):
 
 		self.Bonus_Filling(self.FRACTIONAL_NUMBER_COMMA_EDIT)
 		# Проверяем сохранилась ли преми и не появилось ли ошибок
-		time.sleep(0.5)
 		self.assertNotEqual(self.driver.title, u'Расчет премии')
 
-		time.sleep(1)
 		# Извлечение значения строки с премией
 		SUMM_AMOUNT = self.driver.find_element(By.ID, self.SUMM_OF_BONUS).get_attribute('value')
 		# Извлечение целого значения
@@ -220,11 +218,9 @@ class BonusTest(unittest.TestCase):
 		# Архивация премии
 		self.driver.find_element_by_partial_link_text(self.IN_ARCHIVE).click()
 		# Переключение на всплывающее окно  и подтверждение действия по архивации премии
-		time.sleep(1)
 		self.driver.switch_to.alert.accept()
 
 	def tearDown(self):
-		time.sleep(1)
 		self.driver.quit()
 
 if __name__ == '__main__':
